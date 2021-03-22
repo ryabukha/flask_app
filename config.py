69f7638a51db530a420094ee643728bf2bbbe639
ryabukha <s.ryabukha@gmail.com>
2021-03-22 18:27:38 +0200

    reset pass 9

--- config.py
+++ config.py
@@ -12,4 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['p-xa@ukr.net']
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 10
